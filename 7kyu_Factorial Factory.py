103f2e1f3f01ef8a0ca5c6cf40074eca24968c03
fae <70903778+fae-ld@users.noreply.github.com>
2021-03-18 20:19:31 +0700

    Update 7kyu_Factorial Factory.py

--- 7kyu_Factorial Factory.py
+++ 7kyu_Factorial Factory.py
@@ -1,4 +1,6 @@
 # https://www.codewars.com/kata/528e95af53dcdb40b5000171
+
+# Factorial Factory -- Codewars -- 7 kyu
 #In mathematics, the factorial of integer 'n' is written as 'n!'.
 # It is equal to the product of n and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
 
