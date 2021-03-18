3c3c97b4aea4291dbad789aba280fd330eb8f65b
fae <70903778+fae-ld@users.noreply.github.com>
2021-03-18 20:22:50 +0700

    Update 8kyu_Area of a Square.py

--- 8kyu_Area of a Square.py
+++ 8kyu_Area of a Square.py
@@ -1,5 +1,6 @@
 # https://www.codewars.com/kata/5748838ce2fab90b86001b1a
 
+# Area of a Square -- Codewars -- 8kyu
 # Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
 #
 # Graph
