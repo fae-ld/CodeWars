2e6cbac40d2b2601da5aeb64c5c6979f93e466e0
fae <70903778+fae-ld@users.noreply.github.com>
2021-03-19 18:44:45 +0700

    Add files via upload

---
+++ 8kyu_Remove String Spaces.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/
+
+# Remove String Spaces -- Codewars -- 8kyu
+# Simple, remove the spaces from the string, then return the resultant string.
+
+def no_space(x):
+    return ''.join(x.split(' '))
